e5adec16a8f123572afaf56675e683dd5d8add7e
pytho <pytho@MSI>
2020-07-19 23:30:11 -0500

    Dice

---
+++ src/nsideddice/NSidedDiceRun.java
@@ -0,0 +1,17 @@
+
+package nsideddice;
+
+public class NSidedDiceRun {
+
+
+    public static void main(String[] args) {
+        Die d12 = new Die(12); //makes 12 sides die
+        Die normal = new Die(6); //regular die  
+        System.out.println(d12.getValue());
+        System.out.println(d12.getValue());
+    }
+    
+    
+          
+    }
+
